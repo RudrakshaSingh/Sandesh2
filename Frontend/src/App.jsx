e97b43cd870cf1f62f3034e5cfbfe121aef9d191
Thakur Rudraksha Singh <rudrakshasingh@gmail.com>
2025-04-10 14:11:44 +0530

    userProfile Corrected logic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,11 +5,11 @@ import { Route, Routes } from "react-router-dom";
 
 import CreateCard from './Pages/CreateCard';
 import Home from './Pages/Home';
-import ProfilePage from './Pages/User/ProfilePage';
 import UserForgotPassword from './Pages/User/UserForgotPassword';
 import UserLogin from './Pages/User/UserLogin';
 import UserNewPassword from './Pages/User/UserNewPassword';
 import UserPrivacyPolicy from './Pages/User/UserPrivacyPolicy';
+import UserProfilePage from './Pages/User/UserProfilePage';
 import UserRegister from './Pages/User/UserRegister';
 import { checkAuthStatus } from './Redux/Slices/UserAuth';
 
@@ -30,7 +30,7 @@ function App() {
       <Route path = "/user/new-password" element = {<UserNewPassword/>}/>
       <Route path = "/user/privacy-policy" element = {<UserPrivacyPolicy/>}/>
       <Route path = '/create-card' element  = {<CreateCard/>}/>
-      <Route path = '/user/profile' element = {<ProfilePage/>}/>
+      <Route path = '/user/profile' element = {<UserProfilePage/>}/>
     </Routes>
   )
 }
--- Frontend/src/Pages/User/UserForgotPassword.jsx
+++ Frontend/src/Pages/User/UserForgotPassword.jsx
@@ -1,5 +1,5 @@
+import { KeyRound,Mail } from 'lucide-react'
 import React, { useState } from 'react'
-import { Mail, KeyRound } from 'lucide-react'
 import {Link} from 'react-router-dom';
 
 function UserForgotPassword() {
