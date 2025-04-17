c77ba0354a5cd63de64adcba3d59714855f311ee
Thakur Rudraksha Singh <rudrakshasingh587@gmail.com>
2025-04-17 20:18:54 +0530

    fix: add contract frontend major bug fix

--- Backend/Models/contact.model.js
+++ Backend/Models/contact.model.js
@@ -13,7 +13,7 @@ const contactSchema = new mongoose.Schema(
             required: true,
             trim: true,
         },
-        phone: {
+        mobileNumber: {
             type: String,
             trim: true,
         },
